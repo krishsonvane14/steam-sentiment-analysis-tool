1f56dc517302873cf41f55763e781a92570eca28
dinherr <dinhhenry@hotmail.com>
2025-11-19 18:46:49 -0800

    updated model

--- src/train_model.py
+++ src/train_model.py
@@ -1,9 +1,9 @@
-
 import jsonlines
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report, ConfusionMatrixDisplay
 import torch
 from torch import nn
 from torch import optim
@@ -11,58 +11,48 @@ import scipy
 import numpy
 import matplotlib
 import matplotlib.pyplot as plt
-
+import time
 import sys
 
 
-def train_model(json_filepath):
+class Sequential(nn.Module):
+    def __init__(self, input_dim):
+        super().__init__()
+
+        self.lin1 = nn.Linear(input_dim, 64)
+        self.relu = nn.ReLU()
+        self.lin2 = nn.Linear(64, 2)
+        self.lsm = nn.LogSoftmax(dim=1)
+
+
+    def forward(self, x):
+        x = self.lin1(x)
+        x = self.relu(x)
+        x = self.lin2(x)
+        x = self.lsm(x)
+        return x
+
+def train_model(filepath):
     print("starting operations\n")
-    full_df = pd.DataFrame()
-    count = 0
-    # convert jsonl to dataframe objects to store in full_df
-    with jsonlines.open(json_filepath) as reader:
-        
-        stop = 200
-        for obj in reader:
-            
-            if count == stop:
-                break;
-            
-            df = pd.DataFrame(obj)
-            # drop other rows
-            df = df.drop(['steamid','num_games_owned','num_reviews','playtime_forever','playtime_last_two_weeks','last_played'])
-            # drop columns
-            #df = df.drop(['recommendationid', 'author', 'language', 'timestamp_created', 'timestamp_updated', 'votes_funny',
-            #              'comment_count', 'primarily_steam_deck'], axis=1)
-            df = df[['review', 'voted_up']]
 
-            full_df = pd.concat([full_df, df], ignore_index=True)
-            count = count + 1
+    full_df = pd.read_csv(filepath)
+    full_df['encoded_senti'] = LabelEncoder().fit_transform(full_df['sentiment'])
 
-        print("finished appending \n")
-        full_df['encoded_vote'] = LabelEncoder().fit_transform(full_df['voted_up'])
+    # splitting the dataset up
+    noRPG_df = full_df.loc[full_df['genre'] != "RPG"]
+    onlyRPG_df = full_df.loc[full_df['genre'] == "RPG"]
+    neg_df = noRPG_df.loc[noRPG_df['encoded_senti'] == 0]
+    pos_df = noRPG_df.loc[noRPG_df['encoded_senti'] == 1]
+    pos_df = pos_df.sample(n=neg_df.shape[0], random_state=42)
 
-    # clean up the review text for the vectorizer
-    full_df['review'] = full_df['review'].str.lower()
-    full_df['review'] = full_df['review'].str.replace("[h1]", ' ').str.replace("[/h1]", ' ').str.replace(
-                        "[h2]", ' ').str.replace("[/h2]", ' ').str.replace(
-                        "[b]", ' ').str.replace("[/b]", ' ')
-    
-    
-    '''
-    # check what the review text looks like
-    print(full_df['review'].str.split().apply(len).describe())
-    print("review one\n")
-    print(full_df[full_df['review'].str.split().apply(len) < 90]['review'].values[0])
-    
-    print("review two\n")
-    print(full_df[full_df['review'].str.split().apply(len) > 130]['review'].values[0][:500])
-    '''
+    np_df = [neg_df, pos_df]
+    train_df = pd.concat(np_df)
 
     # TRAINING PREPARATION
     # split into train and test sets with random seed 42
+    
     x_train, x_test, y_train, y_test = train_test_split(
-        full_df['review'], full_df['encoded_vote'], test_size=.2, stratify=full_df['voted_up'],
+        train_df['cleaned_review'], train_df['encoded_senti'], test_size=.3, stratify=train_df['sentiment'],
         random_state=42
     )
     #print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
@@ -70,43 +60,43 @@ def train_model(json_filepath):
     print("vectorizing\n")
     
     # convert x_train and x_test to vectors to tensors
-    vectorizer = TfidfVectorizer()
+    vectorizer = TfidfVectorizer(max_features=3000)
     x_train = vectorizer.fit_transform(x_train)
     x_test = vectorizer.transform(x_test)
 
-    x_train = torch.tensor(scipy.sparse.csr_matrix.todense(x_train)).float()
-    x_test = torch.tensor(scipy.sparse.csr_matrix.todense(x_test)).float()
+    x_train = torch.tensor(x_train.toarray()).float()
+    x_test = torch.tensor(x_test.toarray()).float()
 
     y_train = torch.tensor(y_train.values)
     y_test = torch.tensor(y_test.values)
 
-    # linear regression
-    model = nn.Sequential(
-        nn.Linear(x_train.shape[1], 64),
-        nn.ReLU(),
-        nn.Linear(64, 2),
-        nn.LogSoftmax(dim=1)
-    )
+    print(x_test.shape)
+    print(y_test.shape)
 
+    # linear regression
+    input_dim = x_train.shape[1]
+    model = Sequential(input_dim)
     criterion = nn.NLLLoss()
 
-    fPass = model(x_train)
-    loss = criterion(fPass, y_train)
-
-    loss.backward()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=0.00075)
 
     train_losses = []
     test_losses = []
     test_accuracies = []
+    pred_label = []
+
+    print("training and eval: \n")
+    start_time = time.time()
 
     # training and evaluation
-    epochs = 30
+    epochs = 20
     for e in range(epochs):
+        model.train()
         optimizer.zero_grad()
 
         output = model.forward(x_train)
         loss = criterion(output, y_train)
+
         loss.backward()
         train_loss = loss.item()
         train_losses.append(train_loss)
@@ -125,38 +115,78 @@ def train_model(json_filepath):
             test_accuracy = torch.mean(equals.float())
             test_accuracies.append(test_accuracy)
 
-        model.train()
-
         print(f"Epoch: {e+1}/{epochs}.. ",
             f"Training Loss: {train_loss:.3f}.. ",
             f"Test Loss: {test_loss:.3f}.. ",
             f"Test Accuracy: {test_accuracy:.3f}")
-        
+    
+    print("time to train and eval: ", str(time.time() - start_time))
+    
+    #testing on rpg
+    '''
+    rpgx_train, rpgx_test, rpgy_train, rpgy_test = train_test_split(
+        onlyRPG_df['cleaned_review'], onlyRPG_df['encoded_senti'], test_size=11382, stratify=onlyRPG_df['sentiment'],
+        random_state=42
+    )
 
+    rpgx_train = vectorizer.fit_transform(rpgx_train)
+    rpgx_test = vectorizer.transform(rpgx_test)
 
+    rpgx_train = torch.tensor(rpgx_train.toarray()).float()
+    rpgx_test = torch.tensor(rpgx_test.toarray()).float()
 
-    plt.figure(figsize=(12, 5))
-    ax = plt.subplot(121)
-    plt.xlabel('epochs')
-    plt.ylabel('negative log likelihood loss')
-    plt.plot(train_losses, label='Training loss')
-    plt.plot(test_losses, label='Validation loss')
-    plt.show()
-    plt.legend(frameon=False);
-    plt.subplot(122)
-    plt.xlabel('epochs')
-    plt.ylabel('test accuracy')
-    plt.plot(test_accuracies)
-    plt.show()
-    # FOR FINAL TOOL:
-    # look towards cross validation
-    # how much review data we need to consider
-    # include confusion matrix
-    # time the training sessions
+    rpgy_train = torch.tensor(rpgy_train.values)
+    rpgy_test = torch.tensor(rpgy_test.values)
+
+    print(rpgx_test.shape)
+    print(rpgy_test.shape)
+    '''
+    # confusion matrix
+    with torch.no_grad():
+        model.eval()
+        fPass = model(x_test)
+        test_loss = criterion(fPass, y_test)
+        test_losses.append(test_loss)
+
+        newPass = torch.exp(fPass)
+        _, top_class = newPass.topk(1, dim=1)
+        pred_label = top_class
+        equals = top_class == y_test.view(*top_class.shape)
+        test_accuracy = torch.mean(equals.float())
+        test_accuracies.append(test_accuracy)
+
+    cm = confusion_matrix(y_test, pred_label)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=["Negative", "Positive"])
+    disp.plot(cmap="Blues")
+    plt.title(f"Confusion Matrix")
+    plt.grid(False)
+    plt.savefig('linreg_fig.png')
+
+    # Plot Losses 
+    plt.figure(figsize=(10,4))
+    plt.plot(train_losses, label='Train Loss')
+    plt.plot(test_losses, label='Test Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Training and Test Loss')
+    plt.legend()
+    #plt.show()
+    plt.savefig('loss_plot.png')
+    
+    # Plot Test Accuracy 
+    plt.figure(figsize=(10,4))
+    plt.plot(test_accuracies, label='Test Accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.title('Test Accuracy per Epoch')
+    plt.legend()
+    #plt.show()
+    plt.savefig('test_plot.png')
+    
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python train_model.py <input>")
     else:
-        json_filepath = sys.argv[1]
-        train_model(json_filepath)
+        filepath = sys.argv[1]
+        train_model(filepath)
